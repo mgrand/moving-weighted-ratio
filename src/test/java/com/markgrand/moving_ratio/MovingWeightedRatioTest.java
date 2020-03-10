9a575a53e91cd7ec6b81174301940dcc8ede36cc
Mark Grand <mgrand@markgrand.com>
2020-03-09 21:56:37 -0400

    first real check-in

---
+++ src/test/java/com/markgrand/moving_ratio/MovingWeightedRatioTest.java
@@ -0,0 +1,14 @@
+package com.markgrand.moving_ratio;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovingWeightedRatioTest {
+
+}
