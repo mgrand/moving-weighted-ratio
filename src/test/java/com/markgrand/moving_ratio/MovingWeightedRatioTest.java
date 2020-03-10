9bb1b30dba3e81c36192362dfc3b0e06983c2647
Mark Grand <mgrand@markgrand.com>
2020-03-09 22:15:06 -0400

    Passes unit test

--- src/test/java/com/markgrand/moving_ratio/MovingWeightedRatioTest.java
+++ src/test/java/com/markgrand/moving_ratio/MovingWeightedRatioTest.java
@@ -2,13 +2,48 @@ package com.markgrand.moving_ratio;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.InputStream;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovingWeightedRatioTest {
+    @Test
+    void aOverB() {
+        MovingWeightedRatio mwr = new MovingWeightedRatio();
+        assertEquals(0.0f, mwr.getAOverB());
+        incrementAOverB(mwr);
+        assertEquals(0.5f, mwr.getAOverB());
+        for (int i = 0; i < 100000; i++) {
+            incrementAOverB(mwr);
+        }
+        assertEquals(0.5f, mwr.getAOverB());
+    }
 
+    private void incrementAOverB(final MovingWeightedRatio mwr) {
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+    }
+
+    @Test
+    void bOverA() {
+        MovingWeightedRatio mwr = new MovingWeightedRatio();
+        assertEquals(0.0f, mwr.getBOverA());
+        incrementBOverA(mwr);
+        assertEquals(0.5f, mwr.getBOverA());
+        for (int i = 0; i < 100000; i++) {
+            incrementBOverA(mwr);
+        }
+        assertEquals(0.5f, mwr.getBOverA());
+    }
+
+    private void incrementBOverA(final MovingWeightedRatio mwr) {
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+    }
 }
