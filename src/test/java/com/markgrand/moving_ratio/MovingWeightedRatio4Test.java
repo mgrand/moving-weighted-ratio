c4c4ba9c1f2899410a1d948e9f6c30fde5dd1758
Mark Grand <mgrand@markgrand.com>
2020-03-12 18:52:27 -0400

    Passes all unit tests

--- src/test/java/com/markgrand/moving_ratio/MovingWeightedRatio4Test.java
+++ src/test/java/com/markgrand/moving_ratio/MovingWeightedRatio4Test.java
@@ -90,34 +90,171 @@ class MovingWeightedRatio4Test {
     }
 
     @Test
-    void getBOverC() {
+    void bOverC() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getBOverC());
+        incrementBOverC(mwr);
+        assertEquals(0.5f, mwr.getBOverC());
+        for (int i = 0; i < 100000; i++) {
+            incrementBOverC(mwr);
+        }
+        assertEquals(0.5f, mwr.getBOverC());
+    }
+
+    private void incrementBOverC(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
     }
 
     @Test
-    void getBOverD() {
+    void bOverD() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getBOverD());
+        incrementBOverD(mwr);
+        assertEquals(0.5f, mwr.getBOverD());
+        for (int i = 0; i < 100000; i++) {
+            incrementBOverD(mwr);
+        }
+        assertEquals(0.5f, mwr.getBOverD());
+    }
+
+    private void incrementBOverD(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
     }
 
     @Test
-    void getCOverA() {
+    void cOverA() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getCOverA());
+        incrementCOverA(mwr);
+        assertEquals(0.5f, mwr.getCOverA());
+        for (int i = 0; i < 100000; i++) {
+            incrementCOverA(mwr);
+        }
+        assertEquals(0.5f, mwr.getCOverA());
+    }
+
+    private void incrementCOverA(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
     }
 
     @Test
-    void getCOverB() {
+    void cOverB() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getCOverB());
+        incrementCOverB(mwr);
+        assertEquals(0.5f, mwr.getCOverB());
+        for (int i = 0; i < 100000; i++) {
+            incrementCOverB(mwr);
+        }
+        assertEquals(0.5f, mwr.getCOverB());
+    }
+
+    private void incrementCOverB(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
     }
 
     @Test
-    void getCOverD() {
+    void cOverD() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getCOverD());
+        incrementCOverD(mwr);
+        assertEquals(0.5f, mwr.getCOverD());
+        for (int i = 0; i < 100000; i++) {
+            incrementCOverD(mwr);
+        }
+        assertEquals(0.5f, mwr.getCOverD());
+    }
+
+    private void incrementCOverD(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
     }
 
     @Test
-    void getDOverA() {
+    void dOverA() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getDOverA());
+        incrementDOverA(mwr);
+        assertEquals(0.5f, mwr.getDOverA());
+        for (int i = 0; i < 100000; i++) {
+            incrementDOverA(mwr);
+        }
+        assertEquals(0.5f, mwr.getDOverA());
+    }
+
+    private void incrementDOverA(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+        mwr.incrementCounterA();
+    }
+
+
+    @Test
+    void dOverB() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getDOverB());
+        incrementDOverB(mwr);
+        assertEquals(0.5f, mwr.getDOverB());
+        for (int i = 0; i < 100000; i++) {
+            incrementDOverB(mwr);
+        }
+        assertEquals(0.5f, mwr.getDOverB());
+    }
+
+    private void incrementDOverB(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
+        mwr.incrementCounterB();
     }
 
     @Test
-    void getDOverB() {
+    void dOverC() {
+        MovingWeightedRatio4 mwr = new MovingWeightedRatio4();
+        assertEquals(0.0f, mwr.getDOverC());
+        incrementDOverC(mwr);
+        assertEquals(0.5f, mwr.getDOverC());
+        for (int i = 0; i < 100000; i++) {
+            incrementDOverC(mwr);
+        }
+        assertEquals(0.5f, mwr.getDOverC());
     }
 
-    @Test
-    void getDOverC() {
+    private void incrementDOverC(final MovingWeightedRatio4 mwr) {
+        mwr.incrementCounterD();
+        mwr.incrementCounterD();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
+        mwr.incrementCounterC();
     }
 }
